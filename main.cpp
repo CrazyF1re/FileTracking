937efe7e69d39494b1dd2819b9320b16053606d1
Vova <vova.plisko@mail.ru>
2023-03-23 14:16:08 +0700

    Fix StateFile class and others
    add right connections, output information with standart library cout

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <QCoreApplication>
 #include "monitor.h"
-#include "statefile.h"
-#include <QThread>
+#include "output.h"
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
     Monitor eye;
+    output out;
+    QObject::connect(&eye,&Monitor::Resized,&out,&output::outResizedFile);
+    QObject::connect(&eye,&Monitor::Exist,&out,&output::outExistFile);
+    QObject::connect(&eye,&Monitor::NotExist,&out,&output::outNonExistFile);
+    QObject::connect(&eye,&Monitor::FirstOut,&out,&output::FirstOut);
     eye.AddFile("C:\\Users\\Vova\\Downloads\\qt.txt");
     eye.AddFile("C:\\Users\\Vova\\Downloads\\123.txt");
     eye.AddFile("C:\\Users\\Vova\\Downloads\\file3.txt");
--- output.cpp
+++ output.cpp
@@ -1,31 +1,27 @@
 #include "output.h"
 
-
-output::output()
+void output::outExistFile(QString temp, qint64 size)
 {
-
-}
-void  output::FirstOut(QString path, qint64 size, bool isExist)
-{
-    if (!isExist){
-        printf_s("File %s does not exist\n", path.toStdString().c_str());
-    }
-    else
-    {
-        printf_s("File %s exist, size: %d\n",path.toStdString().c_str(),size);
-    }
-}
-
-void output::outExistFile(QString temp)
-{
-
-    printf_s("File %s exists, size: (((\n", temp.toStdString().c_str());
+    std::cout<< "File "<<temp.toStdString().c_str()<< " exists, size: "<< size<<std::endl;
 }
 void output::outNonExistFile(QString temp)
 {
-    printf_s("File %s does not exist\n", temp.toStdString().c_str());
+    std::cout<<"File "<< temp.toStdString().c_str()<< " does not exist"<<std::endl;
 }
 void output::outResizedFile(QString temp, qint64 size)
 {
-    printf_s("File %s changed size, new_size: %d\n",temp.toStdString().c_str(),size);
+    std::cout<<"File "<< temp.toStdString().c_str()<<" changed size, new_size: "<<size<<std::endl;
+}
+void output::FirstOut(QString path, qint64 size, bool isExist)
+{
+    if (isExist)
+    {
+        std::cout<<"File "<< path.toStdString().c_str()<<" exist, size: " <<size<<std::endl;
+
+    }
+    else
+    {
+        std::cout<<"File "<<path.toStdString().c_str()<<" does not exist"<<std::endl;
+
+    }
 }
